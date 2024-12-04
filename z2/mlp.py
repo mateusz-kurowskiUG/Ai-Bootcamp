ff7f653ec2b091c750960f0fbbdc713cc1bd5214
Mateusz Kurowski <m.kurowski.023@studms.ug.edu.pl>
2024-12-04 18:56:19 +0100

    normalnie na repo df wlecial, GH wytrzyma

--- z2/mlp.py
+++ z2/mlp.py
@@ -2,12 +2,14 @@ from torch import nn
 import torch
 import torch.nn.functional as F
 import polars as pl
-import numpy as np
 from rich import print
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import torch.optim as optim
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Check if GPU is available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -16,16 +18,16 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.input = nn.Linear(2, 20)
-        self.hidden1 = nn.Linear(20, 40)
-        self.hidden2 = nn.Linear(40, 20)
-        self.output = nn.Linear(20, 1)
+        self.input = nn.Linear(2, 200)
+        self.hidden1 = nn.Linear(200, 400)
+        self.hidden2 = nn.Linear(400, 200)
+        self.output = nn.Linear(200, 1)
 
     def forward(self, x):
         x = F.relu(self.input(x))
         x = F.relu(self.hidden1(x))
         x = F.relu(self.hidden2(x))
-        x = self.output(x)  # Remove ReLU for output layer if regression
+        x = self.output(x)
         return x
 
 
@@ -50,23 +52,22 @@ if __name__ == "__main__":
     # Initialize and move model to device
     model = Model().to(device)
 
-    # Split the data into features (x) and target (y)
     x, y = (
-        tensor_data[:, 1:],  # Selects all rows, and columns 1 to end for features
-        tensor_data[:, 0],  # Selects all rows, and column 0 for target
+        tensor_data[:, 1:],
+        tensor_data[:, 0],
     )
 
     # Split data into training and testing sets
     train_x, test_x, train_y, test_y = train_test_split(
-        x, y, test_size=0.3, random_state=42, shuffle=True
+        x, y, test_size=0.25, random_state=42, shuffle=True
     )
 
-    n_epochs = 50
-    batch_size = 16
+    n_epochs = 100
+    batch_size = 32
     batches_per_epoch = len(train_x) // batch_size
-    # loss function and optimizer
-    loss_fn = nn.MSELoss()  # mean square error
-    optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+    loss_fn = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     for epoch in range(n_epochs):
         model.train()
@@ -76,11 +77,11 @@ if __name__ == "__main__":
 
             x_batch = train_x[start:end]
             y_batch = train_y[start:end]
+            optimizer.zero_grad()
             y_pred = model(x_batch)
             loss = loss_fn(
                 y_pred, y_batch.unsqueeze(1)
             )  # Ensure y_batch is of shape [batch_size, 1]
-            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
@@ -97,9 +98,16 @@ if __name__ == "__main__":
         # Additional evaluation metrics
         test_pred_np = test_pred.cpu().numpy()  # Convert to NumPy array for sklearn
         test_y_np = test_y.cpu().numpy()  # Convert to NumPy array for sklearn
+
+        # unstand_test_pred = scaler.inverse_transform(test_pred)
+        # unstand_test_y = scaler.inverse_transform(test_y_np)
+
         mse = mean_squared_error(test_y_np, test_pred_np)
         mae = mean_absolute_error(test_y_np, test_pred_np)
         r2 = r2_score(test_y_np, test_pred_np)
+        # mse = mean_squared_error(unstand_test_y, unstand_test_pred)
+        # mae = mean_absolute_error(unstand_test_y, unstand_test_pred)
+        # r2 = r2_score(unstand_test_y, unstand_test_pred)
 
         print(f"Mean Squared Error (MSE): {mse:.4f}")
         print(f"Mean Absolute Error (MAE): {mae:.4f}")
